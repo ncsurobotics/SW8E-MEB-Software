85036f86085013efbe62ea3ae909b89769df69c9
Marcus Behel <marcus.behel@mindspring.com>
2022-02-28 22:56:50 -0500

    Implemented shutdown and shutdown LED function

--- SW8E-MEB-Software.ino
+++ SW8E-MEB-Software.ino
@@ -19,6 +19,7 @@
 #define TEMP_LED    RED_LED         // Overtemperature LED
 #define LEAK_LED    BLU_LED         // Leak detected LED
 #define THRU_LED    YLW_LED         // Thruster status LED
+#define SDOWN_LED   WHT_LED         // Shutdown indicator LED
 
 // Note: P1_5, P1_6, P1_7 reserved for SPI
 // Note: P2_0, P2_1, P2_2 reserved for I2C
@@ -29,24 +30,31 @@
 
 #define LEAK_DETECT_TIME    250     // How long a leak must be detected for (in ms) to trigger shutdown
 #define LEAK_SHUTDOWN_TIME  10000   // How long to delay shutdown after leak detected
+#define SDOWN_IND_BLINK     250     // Shutdown indicator blink rate (period in ms)
+
+// Shutdown LED behavior (choose one)
+// #define SDOWN_LED_ON             // Shutdown LED is turned on when shutdown scheduled
+// #define SDOWN_LED_BLINK             // Shutdown blinks only its own LED (SDOWN_LED) once shutdown scheduled
+#define SDOWN_BLINK_ALL          // Shutdown blinks all LEDs once shutdown scheduled
 
 
 ///////////////////////////////////////////////////////////////////////////////
 /// Globals
 ///////////////////////////////////////////////////////////////////////////////
 
-unsigned long leakStart;      // When leak first detected
-bool prevLeak;                // Was leak detected last iteration
+unsigned long leakStart;        // When leak first detected
+bool prevLeak;                  // Was leak detected last iteration
 
-unsigned long shutdownTime;   // Time to shutdown at (or after) millis
-bool shutdownEnable;          // Set true to shutdown at given time
+unsigned long shutdownTime;     // Time to shutdown at (or after) millis
+bool shutdownEnable;            // Set true to shutdown at given time
+unsigned long shutdownLedBlink; // Next time to toggle shutdown blink at 
 
 
 ///////////////////////////////////////////////////////////////////////////////
 /// Functions
 ///////////////////////////////////////////////////////////////////////////////
 
-void setup() {
+void setup() {  
   // Setup GPIO pins
   pinMode(SYS_POWER, OUTPUT);
   pinMode(KILL_STAT, INPUT);
@@ -63,7 +71,7 @@ void setup() {
   digitalWrite(TEMP_LED, LOW);        // Overtemp LED off by default
   digitalWrite(LEAK_LED, LOW);        // Leak LED off by default
   digitalWrite(THRU_LED, LOW);        // Thruster kill status LED off by default 
-  digitalWrite(WHT_LED, LOW);         // Currently unused white LED off by default
+  digitalWrite(SDOWN_LED, LOW);       // Currently unused white LED off by default
 
   // Leak detection initial values
   leakStart = 0;
@@ -81,6 +89,9 @@ void loop() {
   // Update thruster kill status LED
   // Thruster kill status signal low = thrusters armed (relay coil- grounded)
   // LED should be on when thrusters armed
+#if defined(SDOWN_BLINK_ALL)
+  if(!shutdownEnable)
+#endif
   digitalWrite(THRU_LED, !digitalRead(KILL_STAT));
 
 
@@ -97,12 +108,38 @@ void loop() {
     if(!shutdownEnable && now - leakStart > LEAK_DETECT_TIME){
       shutdownTime = now + LEAK_SHUTDOWN_TIME;
       shutdownEnable = true;
+      shutdownLedBlink = now;
+      digitalWrite(LEAK_LED, HIGH);
     }
   }
   prevLeak = leak;
 
+
+  // Shutdown if one is scheduled
+  if(shutdownEnable){
+    if(now >= shutdownTime) {
+      digitalWrite(SYS_POWER, LOW); // Actually shutdown
+      while(1);
+    }
+    if(now >= shutdownLedBlink){
+      int newState = !digitalRead(SDOWN_LED);
+      shutdownLedBlink += SDOWN_IND_BLINK;
+#if defined(SDOWN_LED_BLINK)
+      digitalWrite(SDOWN_LED, newState);
+#elif defined(SDOWN_BLINK_ALL)
+      digitalWrite(SYS_LED, newState);
+      digitalWrite(TEMP_LED, newState);
+      digitalWrite(LEAK_LED, newState);
+      digitalWrite(THRU_LED, newState);
+      digitalWrite(SDOWN_LED, newState);
+#endif
+    }
+    #if defined(SDOWN_LED_ON)
+      digitalWrite(SDOWN_LED, HIGH);
+    #endif
+  }
+
   // Slow down execution. Better to use sleep mode, but...
   // No timings less than millisecond level are used so delay 1ms min between iterations
   delay(1);
-  
 }
