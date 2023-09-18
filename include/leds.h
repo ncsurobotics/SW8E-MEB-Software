a2e44f4ea2dd6819a71d47bda7d1d3758af87de8
wpkelso <wkelso02@gmail.com>
2023-09-18 12:49:31 -0400

    Added defines for LED colors

---
+++ include/leds.h
@@ -0,0 +1,25 @@
+/**
+ * @file leds.h
+ * @author William Kelso (wpkelso)
+ * @version 0.1
+ * 
+ * @section DESCRIPTION
+ *
+ */
+
+#ifndef LEDS_H
+#define LEDS_H
+
+//---------------
+// COLORS
+//---------------
+/* Colors are defined in the format (0xRRGGBB) */
+#define BLACK       (0x000000) // Turns the LED OFF
+#define WHITE       (0xFFFFFF)
+#define RED         (0xFF0000)
+#define YELLOW      (0xF08000)
+#define GREEN       (0x00FF00)
+#define BLUE        (0x0000FF)
+#define MAGENTA     (0xFF005E)
+
+#endif
