b28a14fc1e9b3e51fcf6179206612edb5805e9e1
wpkelso <wkelso02@gmail.com>
2023-09-18 16:04:13 -0400

    Migrated current LED functions templates

--- include/leds.h
+++ include/leds.h
@@ -22,4 +22,52 @@
 #define BLUE        (0x0000FF)
 #define MAGENTA     (0xFF005E)
 
+//---------------
+// MAPPINGS
+//---------------
+#define PH0 (0)
+#define PH1 (1)
+#define PH2 (2)
+#define PH3 (3)
+#define PH4 (4)
+#define PH5 (5)
+#define PH6 (6)
+#define PH7 (7)
+#define PH8 (8)
+
+//---------------
+// FUNCTIONS
+//---------------
+
+/*
+ * Configures the RGB values for a specified LED
+ * @param led 
+ * @param color
+ */
+void leds_set_single(int led, uint32_t color);
+
+/*
+ * Configures the RGB for all LEDs
+ * @param color
+ */
+void leds_set_all(uint32_t color);
+
+/*
+ * Transmits the signals to the LEDs necessary for them to recognize the 1s and 0s as 1s and 0s
+ * @param r
+ * @param g
+ * @param b
+ */
+void leds_write(uint8_t r, uint8_t g, uint8_t b);
+
+/*
+ * Sends the data required to write to the LEDs
+ */
+void leds_update();
+
+/*
+ * Turns off all the LEDs on the strip
+ */
+void leds_reset_all();
+
 #endif
